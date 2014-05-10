b3fb37d5cc09cd685b03afd35b83bf65c973cb11
Richard Kiss <him@richardkiss.com>
2014-05-09 22:11:24 -0700

    Add pay_to_script_prefix.

--- pycoin/networks.py
+++ pycoin/networks.py
@@ -4,24 +4,24 @@ from .serialize import h2b
 from .encoding import EncodingError
 
 NetworkValues = namedtuple('NetworkValues',
-    ('code', 'wif_prefix', 'address_prefix', 'bip32_priv_prefix', 'bip32_pub_prefix',
-     'network_name', 'subnet_name'))
+    ('code', 'wif_prefix', 'address_prefix', 'pay_to_script_prefix',
+        'bip32_priv_prefix', 'bip32_pub_prefix', 'network_name', 'subnet_name'))
 
 NETWORKS = (
     # Bitcoin
-    NetworkValues("BTC", b'\x80', b'\0', h2b("0488ADE4"), h2b("0488B21E"), "Bitcoin", "mainnet"),
+    NetworkValues("BTC", b'\x80', b'\0', b'\5', h2b("0488ADE4"), h2b("0488B21E"), "Bitcoin", "mainnet"),
 
     # Bitcoin Textnet3
-    NetworkValues("XTN", b'\xef', b'\x6f', h2b("04358394"), h2b("043587CF"), "Bitcoin", "testnet"),
+    NetworkValues("XTN", b'\xef', b'\x6f', b'\xc4', h2b("04358394"), h2b("043587CF"), "Bitcoin", "testnet"),
 
     # Litecoin
-    NetworkValues("LTC", b'\xb0', b'\x30', None, None, "Litecoin", "mainnet"),
+    NetworkValues("LTC", b'\xb0', b'\x30', None, None, None, "Litecoin", "mainnet"),
 
     # Dogecoin
-    NetworkValues("DOGE", b'\x9e', b'\x1e', h2b("02fda4e8"), h2b("02fda923"), "Dogecoin", "mainnet"),
+    NetworkValues("DOGE", b'\x9e', b'\x1e', b'\x16', h2b("02fda4e8"), h2b("02fda923"), "Dogecoin", "mainnet"),
 
     # BlackCoin: unsure about bip32 prefixes; assuming will use Bitcoin's
-    NetworkValues("BLK", b'\x99', b'\x19', h2b("0488ADE4"), h2b("0488B21E"), "Blackcoin", "mainnet"),
+    NetworkValues("BLK", b'\x99', b'\x19', None, h2b("0488ADE4"), h2b("0488B21E"), "Blackcoin", "mainnet"),
 )
 
 # Map from short code to details about that network.
@@ -36,21 +36,31 @@ NETWORK_NAMES = [i.code for i in NETWORKS]
 def network_name_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].network_name
 
+
 def subnet_name_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].subnet_name
+
 
 def full_network_name_for_netcode(netcode):
     network = NETWORK_NAME_LOOKUP[netcode]
     return "%s %s" % (network.network_name, network.subnet_name)
 
+
 def wif_prefix_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].wif_prefix
+
 
 def address_prefix_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].address_prefix
 
+
+def pay_to_script_prefix_for_netcode(netcode):
+    return NETWORK_NAME_LOOKUP[netcode].pay_to_script_prefix
+
+
 def prv32_prefix_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].bip32_priv_prefix
+
 
 def pub32_prefix_for_netcode(netcode):
     return NETWORK_NAME_LOOKUP[netcode].bip32_pub_prefix
@@ -67,6 +77,7 @@ def netcode_and_type_for_data(data):
     INDEX_LIST = [
         ('wif_prefix', "wif"),
         ('address_prefix', "address"),
+        ('pay_to_script_prefix', "p2script"),
         ('bip32_pub_prefix', "pub32"),
         ('bip32_priv_prefix', "prv32"),
     ]
