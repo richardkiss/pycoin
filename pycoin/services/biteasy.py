d025a2383bc74ceb96be82c7c58c5b2a8c482271
Richard Kiss <him@richardkiss.com>
2014-08-20 23:55:27 -0700

    Small improvement to biteasy.py.

--- pycoin/services/biteasy.py
+++ pycoin/services/biteasy.py
@@ -5,8 +5,8 @@ try:
 except ImportError:
     from urllib.request import urlopen, Request
 
-from pycoin.serialize import h2b_rev
-from pycoin.tx import Spendable
+from pycoin.serialize import b2h_rev, h2b, h2b_rev
+from pycoin.tx import Spendable, Tx, TxIn, TxOut
 from pycoin.tx.script import tools
 
 
@@ -29,3 +29,35 @@ def spendables_for_address(bitcoin_address):
             previous_index = tx_out_info.get("transaction_index")
             spendables.append(Spendable(coin_value, script, previous_hash, previous_index))
     return spendables
+
+def tx_for_tx_hash(tx_hash):
+    URL = "https://api.biteasy.com/blockchain/v1/transactions/%s" % b2h_rev(tx_hash)
+    r = Request(URL,
+                headers={"content-type": "application/json", "accept": "*/*", "User-Agent": "curl/7.29.0" })
+    d = urlopen(r).read()
+    tx = json_to_tx(d.decode("utf8"))
+    if tx.hash() == tx_hash:
+        return tx
+    return None
+
+
+def json_to_tx(json_text):
+    # transactions with non-standard lock time are not decoded properly
+    # for example, d1ef46055a84fd02ee82580d691064780def18614d98646371c3448ca20019ac
+    # there is no way to fix this until biteasy add a lock_time field to their output
+    d = json.loads(json_text).get("data")
+    txs_in = []
+    for d1 in d.get("inputs"):
+        previous_hash = h2b_rev(d1.get("outpoint_hash"))
+        previous_index = d1.get("outpoint_index")
+        script = h2b(d1.get("script_sig"))
+        sequence = 4294967295 # BRAIN DAMAGE
+        txs_in.append(TxIn(previous_hash, previous_index, script, sequence))
+    txs_out = []
+    for d1 in d.get("outputs"):
+        coin_value = d1.get("value")
+        script = h2b(d1.get("script_pub_key"))
+        txs_out.append(TxOut(coin_value, script))
+    version = d.get("version")
+    lock_time = 0 # BRAIN DAMAGE
+    return Tx(version, txs_in, txs_out, lock_time)
