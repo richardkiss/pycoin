98ccb13b7167a2331b27a9176d2d88bc0790df4b
Peter D. Gray <peter@conalgo.com>
2014-09-02 21:34:39 -0400

    first pass; before reorg

--- pycoin/networks.py
+++ pycoin/networks.py
@@ -7,23 +7,30 @@ NetworkValues = namedtuple('NetworkValues',
     ('code', 'network_name',
         'wif_prefix', 'address_prefix',
         'bip32_priv_prefix', 'bip32_pub_prefix',
-        'block_class'))
+        'block_class',
+        'msg_magic'            # Constant "strMessageMagic" in C++ source code, in file "main.cpp"
+    ))
 
 NETWORKS = (
     # Bitcoin
-    NetworkValues("BTC", "Bitcoin", b'\x80', b'\0', h2b("0488ADE4"), h2b("0488B21E"), BitcoinBlock),
+    NetworkValues("BTC", "Bitcoin", b'\x80', b'\0', h2b("0488ADE4"), h2b("0488B21E"),
+        BitcoinBlock, 'Bitcoin Signed Message:\n'),
 
     # Bitcoin Textnet3
-    NetworkValues("XTN", "Bitcoin testnet", b'\xef', b'\x6f', h2b("04358394"), h2b("043587CF"), BitcoinTestNetBlock),
+    NetworkValues("XTN", "Bitcoin testnet", b'\xef', b'\x6f', h2b("04358394"), h2b("043587CF"),
+        BitcoinTestNetBlock, 'Bitcoin Signed Message:\n'),
 
     # Litecoin
-    NetworkValues("LTC", "Litecoin", b'\xb0', b'\x30', None, None, LitecoinBlock),
+    NetworkValues("LTC", "Litecoin", b'\xb0', b'\x30', None, None,
+        LitecoinBlock, 'Litecoin Signed Message:\n'),
 
     # Dogecoin
-    NetworkValues("DOGE", "Dogecoin", b'\x9e', b'\x1e', h2b("02fda4e8"), h2b("02fda923"), Block),
+    NetworkValues("DOGE", "Dogecoin", b'\x9e', b'\x1e', h2b("02fda4e8"), h2b("02fda923"),
+        Block, 'Dogecoin Signed Message:\n'),
 
     # BlackCoin: unsure about bip32 prefixes; assuming will use Bitcoin's
-    NetworkValues("BLK", "Blackcoin", b'\x99', b'\x19', h2b("0488ADE4"), h2b("0488B21E"), BlackcoinBlock),
+    NetworkValues("BLK", "Blackcoin", b'\x99', b'\x19', h2b("0488ADE4"), h2b("0488B21E"),
+        BlackcoinBlock, 'BlackCoin Signed Message:\n'),
 )
 
 # Map from short code to details about that network.
@@ -58,6 +65,10 @@ def transaction_class_for_netcode(netcode):
 def block_class_for_netcode(netcode):
     # We need to know what class to use for the blocks.
     return NETWORK_NAME_LOOKUP[netcode].block_class
+
+def msg_magic_for_netcode(netcode):
+    # A short message prefixed with text to be signed/verified by pubkey.
+    return NETWORK_NAME_LOOKUP[netcode].msg_magic
 
 
 
