6b7a3a0ae3afdebacff98316835e377bfecff618
Kefkius <kefkius@mail.com>
2014-11-17 13:09:48 -0500

    add MZC support

--- pycoin/networks.py
+++ pycoin/networks.py
@@ -45,6 +45,9 @@ NETWORKS = (
     # JBS Jumbucks mainnet : jprv/jpub
     NetworkValues("Jumbucks", "mainnet", "JBS", b'\xab', b'\x2b', None, h2b('037a6460'), h2b('037a689a')),
 
+    # MZC Mazacoin mainnet: xprv/xpub
+    NetworkValues("Mazacoin", "mainnet", "MZC", b'\xe0', b'\x32', b'\9', h2b("0488ADE4"), h2b("0488B21E")),
+
 )
 
 
