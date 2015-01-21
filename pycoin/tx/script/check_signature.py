005cef9637d6c187bcec12063e252ca2133bc135
Marek Miller <marek.l.miller@gmail.com>
2015-01-21 20:27:43 +0100

    The exact order of pubkey/signature OP_CHECKMULTISIG evaluation is important for consensus.

--- pycoin/tx/script/check_signature.py
+++ pycoin/tx/script/check_signature.py
@@ -116,6 +116,10 @@ def op_checkmultisig(stack, signature_for_hash_type_f, expected_hash_type, tmp_s
 
         matching_pair = ppp.pop()
         idx = public_pairs.index(matching_pair)
-        public_pairs = public_pairs[:idx] + public_pairs[idx+1:]
+        # The exact order of pubkey/signature evaluation
+        # is important for consensus.  If someday it will
+        # no longer matter, just replace the line below with:
+        # public_pairs = public_pairs[:idx] + public_pairs[idx+1:]
+        public_pairs = public_pairs[idx+1:]
 
     stack.append(sig_ok)
