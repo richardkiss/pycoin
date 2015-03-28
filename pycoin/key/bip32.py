1d74dbb0759bc50b8c6609ee4d2b995a5fcc86bc
Matt Bogosian <mtb19@columbia.edu>
2015-03-28 07:24:24 -0700

    ValueError -> specific custom exceptions (see @kanzure's diff-specific comments on #108)

--- pycoin/key/bip32.py
+++ pycoin/key/bip32.py
@@ -57,7 +57,7 @@ _SUBKEY_VALIDATION_LOG_ERR_FMT = """
 BUY A LOTTO TICKET RIGHT NOW! (And consider giving up your wallet to
 science!)
 
-You have stumbled across and astronomically unlikely scenario. Your HD
+You have stumbled across an astronomically unlikely scenario. Your HD
 wallet contains an invalid subkey. Having access to this information would
 be incredibly valuable to the Bitcoin development community.
 
@@ -69,6 +69,9 @@ instructions on how best to donate it without losing your bitcoins.
 WARNING: DO NOT SEND ANY WALLET INFORMATION UNLESS YOU WANT TO LOSE ALL
 THE BITCOINS IT CONTAINS.
 """.strip()
+
+
+class DerivationError(ValueError): pass
 
 
 def subkey_secret_exponent_chain_code_pair(
@@ -104,11 +107,11 @@ def subkey_secret_exponent_chain_code_pair(
     I_left_as_exponent = from_bytes_32(I64[:32])
     if I_left_as_exponent >= ORDER:
         logger.critical(_SUBKEY_VALIDATION_LOG_ERR_FMT)
-        raise ValueError('bad derviation: I_L >= {}'.format(ORDER))
+        raise DerivationError('I_L >= {}'.format(ORDER))
     new_secret_exponent = (I_left_as_exponent + secret_exponent) % ORDER
     if new_secret_exponent == 0:
         logger.critical(_SUBKEY_VALIDATION_LOG_ERR_FMT)
-        raise ValueError('bad derviation: k_{} == 0'.format(i))
+        raise DerivationError('k_{} == 0'.format(i))
     new_chain_code = I64[32:]
     return new_secret_exponent, new_chain_code
 
@@ -139,12 +142,12 @@ def subkey_public_pair_chain_code_pair(public_pair, chain_code_bytes, i):
         ecdsa.Point(ecdsa.generator_secp256k1.curve(), x, y, ORDER)
     if the_point == INFINITY:
         logger.critical(_SUBKEY_VALIDATION_LOG_ERR_FMT)
-        raise ValueError('bad derviation: K_{} == {}'.format(i, the_point))
+        raise DerivationError('K_{} == {}'.format(i, the_point))
 
     I_left_as_exponent = from_bytes_32(I64[:32])
     if I_left_as_exponent >= ORDER:
         logger.critical(_SUBKEY_VALIDATION_LOG_ERR_FMT)
-        raise ValueError('bad derviation: I_L >= {}'.format(ORDER))
+        raise DerivationError('I_L >= {}'.format(ORDER))
     new_public_pair = the_point.pair()
     new_chain_code = I64[32:]
     return new_public_pair, new_chain_code
