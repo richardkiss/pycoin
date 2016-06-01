7a78679c0917514837da28b43076d7c87ca43ce0
Michael Flaxman <mflaxman@itbit.com>
2016-06-01 10:22:49 -0400

    add serialization test

---
+++ tests/test_serialize.py
@@ -0,0 +1,20 @@
+# python modules
+import unittest
+
+# local modules
+from pycoin.serialize import h2b, h2b_rev, b2h, b2h_rev
+
+
+class SerializeTest(unittest.TestCase):
+
+    def test_h2b(self):
+        h = "000102"
+        b = b"\x00\x01\x02"
+        self.assertEqual(h2b(h), b)
+        self.assertEqual(b2h(b), h)
+        self.assertEqual(h2b_rev(h), b[::-1])
+        self.assertEqual(b2h_rev(b), "020100")
+
+
+if __name__ == '__main__':
+    unittest.main()
