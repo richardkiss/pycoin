3e4122385cb96f4937c71b46e467441f30ccfbea
Richard Kiss <him@richardkiss.com>
2016-07-27 14:41:07 -0700

    Minor update to CHANGES, README.md. Add comments to netcodes.

--- pycoin/networks/netcodes.py
+++ pycoin/networks/netcodes.py
@@ -1,4 +1,5 @@
 from .all import BUILT_IN_NETWORKS
+from .network import Network
 
 
 _NETWORK_NAME_LOOKUP = {}
@@ -8,7 +9,19 @@ _NETWORK_PREFIXES = {}
 _NETWORK_CODES = []
 
 
+def clear_all_networks():
+    global _NETWORK_NAME_LOOKUP, _NETWORK_PREFIXES, _NETWORK_CODES
+    _NETWORK_NAME_LOOKUP = {}
+    _NETWORK_PREFIXES = {}
+    _NETWORK_CODES = []
+
+
 def register_network(network_info):
+    """
+    Accept a Network instance and register it in the database
+    by its netcode.
+    """
+    assert isinstance(network_info, Network)
     code = network_info.code
     if code in _NETWORK_NAME_LOOKUP:
         raise ValueError("code %s already defined" % code)
@@ -28,14 +41,24 @@ def _register_default_networks():
 
 
 def network_for_netcode(netcode):
+    """
+    Return the given Network object for the given netcode (or None).
+    """
     return _NETWORK_NAME_LOOKUP.get(netcode)
 
 
 def network_codes():
+    """
+    Return a list of registered netcodes, in the order they were registered.
+    """
     return _NETWORK_CODES
 
 
 def network_prefixes():
+    """
+    Return a dictionary of 1 and 4 byte prefixes that returns a pair (a, b) where
+    a is the netcode and b is one of "wif", "address", "pay_to_script", "prv32" or "pub32".
+    """
     return _NETWORK_PREFIXES
 
 
@@ -48,36 +71,47 @@ def _lookup(netcode, property):
 
 
 def network_name_for_netcode(netcode):
+    "Return the network name for the given netcode (or None)"
     return _lookup(netcode, "network_name")
 
 
 def subnet_name_for_netcode(netcode):
+    """
+    Return the subnet network name for the given netcode (or None).
+    This is usually "testnet" or "mainnet".
+    """
     return _lookup(netcode, "subnet_name")
 
 
 def full_network_name_for_netcode(netcode):
+    "Return the full network name for the given netcode (or None)"
     network = _NETWORK_NAME_LOOKUP[netcode]
     if network:
         return "%s %s" % (network.network_name, network.subnet_name)
 
 
 def wif_prefix_for_netcode(netcode):
+    "Return the 1 byte prefix for WIFs for the given netcode (or None)"
     return _lookup(netcode, "wif")
 
 
 def address_prefix_for_netcode(netcode):
+    "Return the 1 byte prefix for addresses for the given netcode (or None)"
     return _lookup(netcode, "address")
 
 
 def pay_to_script_prefix_for_netcode(netcode):
+    "Return the 1 byte prefix for pay-to-script addresses for the given netcode (or None)"
     return _lookup(netcode, "pay_to_script")
 
 
 def prv32_prefix_for_netcode(netcode):
+    "Return the 4 byte prefix for private BIP32 addresses for the given netcode (or None)"
     return _lookup(netcode, "prv32")
 
 
 def pub32_prefix_for_netcode(netcode):
+    "Return the 4 byte prefix for public BIP32 addresses for the given netcode (or None)"
     return _lookup(netcode, "pub32")
 
 
