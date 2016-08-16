3f5eb0bb93989cb9a1a25501ec9ef082e24c241e
Richard Kiss <him@richardkiss.com>
2016-08-16 12:01:03 -0400

    Use a base class for blocks.

--- pycoin/base/BaseBlock.py
+++ pycoin/base/BaseBlock.py
@@ -48,10 +48,13 @@ def difficulty_max_mask_for_bits(bits):
     return mask
 
 
-class BlockHeader(object):
-    """A BlockHeader is a block with the transaction data removed. With a
+class BaseBlockHeader(object):
+    """
+    A BlockHeader is a block with the transaction data removed. With a
     complete Merkle tree database, it can be reconstructed from the
-    merkle_root."""
+    merkle_root.
+    Use these attribute names: nonce, txs, __hash, previous_block_hash
+    """
 
     Tx = BaseTx
 
@@ -60,42 +63,35 @@ class BlockHeader(object):
         """Parse the BlockHeader from the file-like object in the standard way
         that blocks are sent in the network (well, except we ignore the
         transaction information)."""
-        (version, previous_block_hash, merkle_root,
-            timestamp, difficulty, nonce) = struct.unpack("<L32s32sLLL", f.read(4+32+32+4*3))
-        return cls(version, previous_block_hash, merkle_root, timestamp, difficulty, nonce)
+        raise NotImplemented()
 
-    def __init__(self, version, previous_block_hash, merkle_root, timestamp, difficulty, nonce):
-        self.version = version
-        self.previous_block_hash = previous_block_hash
-        self.merkle_root = merkle_root
-        self.timestamp = timestamp
-        self.difficulty = difficulty
-        self.nonce = nonce
+    def __init__(self):
+        raise NotImplemented()
 
     def set_nonce(self, nonce):
         self.nonce = nonce
         if hasattr(self, "__hash"):
             del self.__hash
 
+    def _calculate_hash(self):
+        raise NotImplemented()
+
     def hash(self):
         """Calculate the hash for the block header. Note that this has the bytes
         in the opposite order from how the header is usually displayed (so the
         long string of 00 bytes is at the end, not the beginning)."""
         if not hasattr(self, "__hash"):
-            s = io.BytesIO()
-            self.stream_header(s)
-            self.__hash = double_sha256(s.getvalue())
+            self.__hash = self._calculate_hash()
         return self.__hash
 
     def stream_header(self, f):
         """Stream the block header in the standard way to the file-like object f."""
-        stream_struct("L##LLL", f, self.version, self.previous_block_hash,
-                      self.merkle_root, self.timestamp, self.difficulty, self.nonce)
+        raise NotImplemented()
 
     def stream(self, f):
         """Stream the block header in the standard way to the file-like object f.
         The Block subclass also includes the transactions."""
-        self.stream_header(f)
+        return self.stream_header(f)
 
     def as_bin(self):
         """Return the transaction as binary."""
@@ -118,71 +114,31 @@ class BlockHeader(object):
         return b2h_rev(self.previous_block_hash)
 
     def __str__(self):
-        return "BlockHeader [%s] (previous %s)" % (self.id(), self.previous_block_id())
+        return "%s [%s] (previous %s)" % (self.__class__.__name__, self.id(), self.previous_block_id())
 
     def __repr__(self):
-        return "BlockHeader [%s] (previous %s)" % (self.id(), self.previous_block_id())
+        return self.__str__()
 
 
-class Block(BlockHeader):
+class BaseBlock(BaseBlockHeader):
     """A Block is an element of the Bitcoin chain. Generating a block
     yields a reward!"""
 
     @classmethod
     def parse(cls, f, include_offsets=None):
-        """Parse the Block from the file-like object in the standard way
-        that blocks are sent in the network."""
-        if include_offsets is None:
-            include_offsets = hasattr(f, "tell")
-        (version, previous_block_hash, merkle_root, timestamp,
-            difficulty, nonce, count) = parse_struct("L##LLLI", f)
-        txs = []
-        for i in range(count):
-            if include_offsets:
-                offset_in_block = f.tell()
-            tx = cls.Tx.parse(f)
-            txs.append(tx)
-            if include_offsets:
-                tx.offset_in_block = offset_in_block
-        block = cls(version, previous_block_hash, merkle_root, timestamp, difficulty, nonce, txs)
-        for tx in txs:
-            tx.block = block
-        return block
-
-    @classmethod
-    def from_bin(cls, bytes):
-        f = io.BytesIO(bytes)
-        return cls.parse(f)
-
-    def __init__(self, version, previous_block_hash, merkle_root, timestamp, difficulty, nonce, txs):
-        self.version = version
-        self.previous_block_hash = previous_block_hash
-        self.merkle_root = merkle_root
-        self.timestamp = timestamp
-        self.difficulty = difficulty
-        self.nonce = nonce
-        self.txs = txs
-
-    def as_blockheader(self):
-        return BlockHeader(self.version, self.previous_block_hash, self.merkle_root,
-                           self.timestamp, self.difficulty, self.nonce)
+        """Parse the BlockHeader from the file-like object in the standard way
+        that blocks are sent in the network (well, except we ignore the
+        transaction information)."""
+        raise NotImplemented()
 
     def stream(self, f):
-        """Stream the block in the standard way to the file-like object f."""
-        stream_struct("L##LLLI", f, self.version, self.previous_block_hash,
-                      self.merkle_root, self.timestamp, self.difficulty, self.nonce, len(self.txs))
-        for t in self.txs:
-            t.stream(f)
+        raise NotImplemented()
 
-    def as_bin(self):
-        """Return the transaction as binary."""
-        f = io.BytesIO()
-        self.stream(f)
-        return f.getvalue()
+    def __init__(self):
+        raise NotImplemented()
 
-    def as_hex(self):
-        """Return the transaction as hex."""
-        return b2h(self.as_bin())
+    def as_blockheader(self):
+        raise NotImplemented()
 
     def check_merkle_hash(self):
         """Raise a BadMerkleRootError if the Merkle hash of the
@@ -193,9 +149,8 @@ class Block(BlockHeader):
                 "calculated %s but block contains %s" % (b2h(calculated_hash), b2h(self.merkle_root)))
 
     def __str__(self):
-        return "Block [%s] (previous %s) [tx count: %d]" % (
-            self.id(), self.previous_block_id(), len(self.txs))
+        return "%s [%s] (previous %s)" % (self.__class__.__name__, self.id(), self.previous_block_id())
 
     def __repr__(self):
-        return "Block [%s] (previous %s) [tx count: %d] %s" % (
-            self.id(), self.previous_block_id(), len(self.txs), self.txs)
+        return "%s [%s] (previous %s) [tx count:%d] %s" % (
+            self.__class__.__name__, self.id(), self.previous_block_id(), len(self.txs), self.txs)
--- pycoin/networks/bitcoin/Block.py
+++ pycoin/networks/bitcoin/Block.py
@@ -30,7 +30,7 @@ import struct
 
 import io
 
-from pycoin.base.BaseBlock import BadMerkleRootError, difficulty_max_mask_for_bits
+from pycoin.base.BaseBlock import BadMerkleRootError, difficulty_max_mask_for_bits, BaseBlockHeader, BaseBlock
 from pycoin.base.merkle import merkle
 from pycoin.encoding import double_sha256
 from pycoin.serialize.bitcoin_streamer import parse_struct, stream_struct
@@ -40,7 +40,7 @@ from .Tx import Tx
 
 
 
-class BlockHeader(object):
+class BlockHeader(BaseBlockHeader):
     """A BlockHeader is a block with the transaction data removed. With a
     complete Merkle tree database, it can be reconstructed from the
     merkle_root."""
@@ -69,54 +69,17 @@ class BlockHeader(object):
         if hasattr(self, "__hash"):
             del self.__hash
 
-    def hash(self):
-        """Calculate the hash for the block header. Note that this has the bytes
-        in the opposite order from how the header is usually displayed (so the
-        long string of 00 bytes is at the end, not the beginning)."""
-        if not hasattr(self, "__hash"):
-            s = io.BytesIO()
-            self.stream_header(s)
-            self.__hash = double_sha256(s.getvalue())
-        return self.__hash
+    def _calculate_hash(self):
+        s = io.BytesIO()
+        self.stream_header(s)
+        return double_sha256(s.getvalue())
 
     def stream_header(self, f):
         """Stream the block header in the standard way to the file-like object f."""
         stream_struct("L##LLL", f, self.version, self.previous_block_hash,
                       self.merkle_root, self.timestamp, self.difficulty, self.nonce)
 
-    def stream(self, f):
-        """Stream the block header in the standard way to the file-like object f.
-        The Block subclass also includes the transactions."""
-        self.stream_header(f)
-
-    def as_bin(self):
-        """Return the transaction as binary."""
-        f = io.BytesIO()
-        self.stream(f)
-        return f.getvalue()
-
-    def as_hex(self):
-        """Return the transaction as hex."""
-        return b2h(self.as_bin())
-
-    def id(self):
-        """Returns the hash of the block displayed with the bytes in the order
-        they are usually displayed in."""
-        return b2h_rev(self.hash())
-
-    def previous_block_id(self):
-        """Returns the hash of the previous block, with the bytes in the order
-        they are usually displayed in."""
-        return b2h_rev(self.previous_block_hash)
-
-    def __str__(self):
-        return "BlockHeader [%s] (previous %s)" % (self.id(), self.previous_block_id())
-
-    def __repr__(self):
-        return "BlockHeader [%s] (previous %s)" % (self.id(), self.previous_block_id())
-
-
-class Block(BlockHeader):
+class Block(BlockHeader, BaseBlock):
     """A Block is an element of the Bitcoin chain. Generating a block
     yields a reward!"""
 
@@ -155,6 +118,7 @@ class Block(BlockHeader):
         self.difficulty = difficulty
         self.nonce = nonce
         self.txs = txs
+        self.check_merkle_hash()
 
     def as_blockheader(self):
         return BlockHeader(self.version, self.previous_block_hash, self.merkle_root,
@@ -166,29 +130,3 @@ class Block(BlockHeader):
                       self.merkle_root, self.timestamp, self.difficulty, self.nonce, len(self.txs))
         for t in self.txs:
             t.stream(f)
-
-    def as_bin(self):
-        """Return the transaction as binary."""
-        f = io.BytesIO()
-        self.stream(f)
-        return f.getvalue()
-
-    def as_hex(self):
-        """Return the transaction as hex."""
-        return b2h(self.as_bin())
-
-    def check_merkle_hash(self):
-        """Raise a BadMerkleRootError if the Merkle hash of the
-        transactions does not match the Merkle hash included in the block."""
-        calculated_hash = merkle([tx.hash() for tx in self.txs], double_sha256)
-        if calculated_hash != self.merkle_root:
-            raise BadMerkleRootError(
-                "calculated %s but block contains %s" % (b2h(calculated_hash), b2h(self.merkle_root)))
-
-    def __str__(self):
-        return "Block [%s] (previous %s) [tx count: %d]" % (
-            self.id(), self.previous_block_id(), len(self.txs))
-
-    def __repr__(self):
-        return "Block [%s] (previous %s) [tx count: %d] %s" % (
-            self.id(), self.previous_block_id(), len(self.txs), self.txs)
