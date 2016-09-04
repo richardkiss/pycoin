192f82a48a9369f2c53863716e8c0fefd3bda50e
protonn <support@argentum.io>
2016-09-03 21:49:22 -0500

    Add Argentum

--- pycoin/networks.py
+++ pycoin/networks.py
@@ -73,6 +73,9 @@ NETWORKS = (
     # FAI faircoin mainnet : xprv/xpub
     NetworkValues("Faircoin", "mainnet", "FAI", b'\xdf', b'\x5f', b'\x24', h2b("0488ADE4"), h2b("0488B21E")),
 
+    # ARG argentum mainnet : xprv/xpub
+    NetworkValues("Argentum", "mainnet", "ARG", b'\x97', b'\x17', b'\5', h2b("0488ADE4"), h2b("0488B21E")),
+
 )
 
 
