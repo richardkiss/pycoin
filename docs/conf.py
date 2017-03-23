7df390621d98cbabd33989bf53ced6742c7b3ca9
Richard Kiss <him@richardkiss.com>
2017-03-23 16:33:46 -0700

    Change the theme to alabaster and Pycoin => pycoin.

--- docs/conf.py
+++ docs/conf.py
@@ -70,8 +70,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pycoin'
-copyright = u'2013, Richard Kiss'
+project = 'pycoin'
+copyright = '2013, Richard Kiss'
+author = 'Richard Kiss'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -120,7 +121,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -212,8 +213,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'pycoin.tex', u'Pycoin Documentation',
-   u'Richard Kiss', 'manual'),
+  ('index', 'pycoin.tex', 'pycoin Documentation',
+   'Richard Kiss', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -242,8 +243,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pycoin', u'Pycoin Documentation',
-     [u'Richard Kiss'], 1)
+    ('index', 'pycoin', 'pycoin Documentation',
+     ['Richard Kiss'], 1)
 ]
 
 # If true, show URL addresses after external links.
