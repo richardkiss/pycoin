aeebc03ed9b8b6ad4dc68ccceafcf607924f008a
Richard Kiss <him@richardkiss.com>
2017-04-12 19:26:39 -0700

    First crack at revhex.

--- pycoin/serialize/bitcoin_streamer.py
+++ pycoin/serialize/bitcoin_streamer.py
@@ -1,6 +1,7 @@
 
 import struct
 
+from . import bytes_as_revhex
 from .streamer import Streamer
 
 
@@ -42,7 +43,7 @@ STREAMER_FUNCTIONS = {
     "h": (lambda f: struct.unpack("!H", f.read(2))[0], lambda f, v: f.write(struct.pack("!H", v))),
     "L": (lambda f: struct.unpack("<L", f.read(4))[0], lambda f, v: f.write(struct.pack("<L", v))),
     "Q": (lambda f: struct.unpack("<Q", f.read(8))[0], lambda f, v: f.write(struct.pack("<Q", v))),
-    "#": (lambda f: f.read(32), lambda f, v: f.write(v[:32])),
+    "#": (lambda f: bytes_as_revhex(f.read(32)), lambda f, v: f.write(v[:32])),
     "@": (lambda f: f.read(16), lambda f, v: f.write(v[:16])),
     "b": (lambda f: struct.unpack("?", f.read(1))[0], lambda f, b: f.write(struct.pack("?", b))),
 }
