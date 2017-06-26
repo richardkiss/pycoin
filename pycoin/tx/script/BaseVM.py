acb72d794fe6736ff30f4f56fcb00e750c32e2df
Richard Kiss <him@richardkiss.com>
2017-06-26 13:22:35 -0700

    Call prelaunch and postscript in traceback function.

--- pycoin/tx/script/BaseVM.py
+++ pycoin/tx/script/BaseVM.py
@@ -77,8 +77,16 @@ class VM(object):
         if len(vmc.script) > vmc.MAX_SCRIPT_LENGTH:
             raise ScriptError("script too long", errno.SCRIPT_SIZE)
 
+        f = getattr(vmc.traceback_f, "prelaunch", None)
+        if f:
+            f(vmc)
+
         while vmc.pc < len(vmc.script):
             class_.eval_instruction(vmc)
+
+        f = getattr(vmc.traceback_f, "postscript", None)
+        if f:
+            f(vmc)
 
         class_.post_script_check(vmc)
         return vmc.stack
