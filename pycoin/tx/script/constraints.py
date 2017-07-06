d37d7101c61338f88c5c1863efac66bcf8d6a715
Richard Kiss <him@richardkiss.com>
2017-07-06 10:01:37 -0700

    Reduce complexity.

--- pycoin/tx/script/constraints.py
+++ pycoin/tx/script/constraints.py
@@ -58,39 +58,47 @@ class Operator(Atom):
         return "(%s %s)" % (self._op_name, ' '.join(repr(a) for a in self._args))
 
 
-def my_op_if(vm):
-    pdb.set_trace()
-    t = vm.stack.pop()
-    t = Operator('IF', t)
-    vm.stack.append(t)
+def make_op_if(constraints):
+
+    def my_op_if(vm):
+        pdb.set_trace()
+        t = vm.stack.pop()
+        t = Operator('IF', t)
+        vm.stack.append(t)
+    return my_op_if
 
 
-def my_op_hash160(vm):
-    t = vm.stack.pop()
-    t = Operator('HASH160', t)
-    vm.stack.append(t)
+def make_op_hash160(constraints):
+
+    def my_op_hash160(vm):
+        t = vm.stack.pop()
+        t = Operator('HASH160', t)
+        vm.stack.append(t)
+    my_op_hash160.stack_size = 1
+    return my_op_hash160
 
 
-my_op_hash160.stack_size = 1
+def make_op_equal(constraints):
+    def my_op_equal(vm):
+        t1 = vm.stack.pop()
+        t2 = vm.stack.pop()
+        c = Operator('EQUAL', t1, t2)
+        vm.append(c)
+    my_op_equal.stack_size = 2
+    return my_op_equal
 
 
-def my_op_equal(vm):
-    t1 = vm.stack.pop()
-    t2 = vm.stack.pop()
-    c = Operator('EQUAL', t1, t2)
-    vm.append(c)
-
-
-my_op_equal.stack_size = 2
-
-
-def make_traceback_f(constraints, reset_stack_f):
-
+def make_op_equalverify(constraints):
     def my_op_equalverify(vm):
-        my_op_equal(vm)
-        constraints.append(Operator('IS_TRUE', vm.pop()))
+        t1 = vm.stack.pop()
+        t2 = vm.stack.pop()
+        c = Operator('EQUAL', t1, t2)
+        constraints.append(Operator('IS_TRUE', c))
     my_op_equalverify.stack_size = 2
+    return my_op_equalverify
 
+
+def make_op_checksig(constraints):
     def my_op_checksig(vm):
 
         def sighash_f(signature_type):
@@ -102,7 +110,10 @@ def make_traceback_f(constraints, reset_stack_f):
         constraints.append(Operator('IS_PUBKEY', t1))
         constraints.append(Operator('IS_SIGNATURE', t2))
         vm.stack.append(t)
+    return my_op_checksig
 
+
+def make_op_checkmultisig(constraints):
     def my_op_checkmultisig(vm):
 
         def sighash_f(signature_type):
@@ -122,16 +133,20 @@ def make_traceback_f(constraints, reset_stack_f):
         constraints.append(Operator('IS_TRUE', Operator('EQUAL', t1, b'')))
         t = Operator('SIGNATURES_CORRECT', public_pair_blobs, sig_blobs, sighash_f)
         vm.stack.append(t)
+    return my_op_checkmultisig
 
-    MY_OPCODES = {
-        "OP_HASH160": my_op_hash160,
-        "OP_EQUALVERIFY": my_op_equalverify,
-        "OP_EQUAL": my_op_equal,
-        "OP_CHECKSIG": my_op_checksig,
-        "OP_CHECKMULTISIG": my_op_checkmultisig,
-    }
 
-    MY_OPCODES = {BitcoinScriptTools.int_for_opcode(k): v for k, v in MY_OPCODES.items()}
+def make_traceback_f(constraints, reset_stack_f):
+
+    TWEAKED_OPCODES = (
+        ("OP_HASH160", make_op_hash160),
+        ("OP_EQUALVERIFY", make_op_equalverify),
+        ("OP_EQUAL", make_op_equal),
+        ("OP_CHECKSIG", make_op_checksig),
+        ("OP_CHECKMULTISIG", make_op_checkmultisig),
+    )
+
+    MY_OPCODES = {BitcoinScriptTools.int_for_opcode(k): v(constraints) for k, v in TWEAKED_OPCODES}
 
     def prelaunch(vmc):
         if not vmc.is_solution_script:
