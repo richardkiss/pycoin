d69e2bcd8cc66ac219ebfc15a448d4f28011ffb3
Richard Kiss <him@richardkiss.com>
2017-09-27 17:31:48 -0600

    Add crack_k_from_sigs and crack_secret_exponent_from_k, and tests.

--- tests/crack_sig_test.py
+++ tests/crack_sig_test.py
@@ -7,9 +7,71 @@ from pycoin.key.BIP32Node import BIP32Node
 #################
 
 
+def crack_secret_exponent_from_k(generator, signed_value, sig, k):
+    """
+    Given a signature of a signed_value and a known k, return the secret exponent.
+    """
+    r, s = sig
+    return ((s * k - signed_value) * generator.inverse(r)) % generator.order()
+
+
+def crack_k_from_sigs(generator, sig1, val1, sig2, val2):
+    """
+    Given two signatures with the same secret exponent and K value, return that K value.
+    """
+
+    # s1 = v1 / k1 + (se * r1) / k1
+    # s2 = v2 / k2 + (se * r2) / k2
+    # and k = k1 = k2
+    # so
+    # k * s1 = v1 + (se * r1)
+    # k * s2 = v2 + (se * r2)
+    # so
+    # k * s1 * r2 = r2 * v1 + (se * r1 * r2)
+    # k * s2 * r1 = r1 * v2 + (se * r2 * r1)
+    # so
+    # k (s1 * r2 - s2 * r1) = r2 * v1 - r1 * v2
+    # so
+    # k = (r2 * v1 - r1 * v2) / (s1 * r2 - s2 * r1)
+
+    r1, s1 = sig1
+    r2, s2 = sig2
+    if r1 != r2:
+        raise ValueError("r values of signature do not match")
+    k = (r2 * val1 - r1 * val2) * generator.inverse(r2 * s1 - r1 * s2)
+    return k % generator.order()
+
+
+from pycoin.ecdsa.secp256k1 import secp256k1_generator
+
+
+def make_gen_k_const(K):
+
+    def gen_k(*args):
+        return K
+    return gen_k
+
 
 class CrackSigTest(unittest.TestCase):
-    pass
+    def test_crack_secret_exponent_from_k(self):
+        k = 105
+        se = 181919191
+        gen_k = make_gen_k_const(k)
+        val = 488819181819384
+        sig = secp256k1_generator.sign(se, val, gen_k=gen_k)
+        cracked_se = crack_secret_exponent_from_k(secp256k1_generator, val, sig, k)
+        self.assertEqual(cracked_se, se)
+
+    def test_crack_k_from_sigs(self):
+        k = 105
+        se = 181919191
+        gen_k = make_gen_k_const(k)
+        val1 = 488819181819384
+        val2 = 588819181819384
+        sig1 = secp256k1_generator.sign(se, val1, gen_k=gen_k)
+        sig2 = secp256k1_generator.sign(se, val2, gen_k=gen_k)
+        cracked_k = crack_k_from_sigs(secp256k1_generator, sig1, val1, sig2, val2)
+        self.assertEqual(cracked_k, k)
 
 
 import struct
@@ -20,6 +82,7 @@ from pycoin.key.BIP32Node import BIP32Node
 
 
 ORDER = secp256k1_generator.order()
+
 
 def ascend_bip32(bip32_pub_node, secret_exponent, child):
     """
@@ -48,7 +111,6 @@ class CrackBIP32Test(unittest.TestCase):
         bip32key = BIP32Node.from_master_secret(b"foo")
         bip32_pub = bip32key.public_copy()
         secret_exponent_p0_1_7_9 = bip32key.subkey_for_path("0/1/7/9").secret_exponent()
-
         cracked_bip32_node = crack_bip32(bip32_pub, secret_exponent_p0_1_7_9, "0/1/7/9")
         self.assertEqual(cracked_bip32_node.hwif(as_private=True), bip32key.hwif(as_private=True))
 
