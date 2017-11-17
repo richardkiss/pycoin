fbacea7e1ea67eaafef9cfdf652dc8176cb1bba5
Richard Kiss <him@richardkiss.com>
2017-11-17 13:05:56 -0800

    Add some comments.

--- pycoin/coins/Tx.py
+++ pycoin/coins/Tx.py
@@ -1,27 +1,3 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2013 by Richard Kiss
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
 import io
 
 from .SolutionChecker import SolutionChecker, ScriptError
@@ -139,11 +115,22 @@ class Tx(object):
     """
 
     def set_unspents(self, unspents):
+        """
+        Set the unspent inputs for a transaction.
+
+        :param unspents: a list of :class:`TxOut` (or the subclass :class:`Spendable`) objects
+            corresponding to the :class:`TxIn` objects for this transaction (same number of
+            items in each list)
+        """
         if len(unspents) != len(self.txs_in):
             raise ValueError("wrong number of unspents")
         self.unspents = unspents
 
     def sign(self, *args, **kwargs):
+        """
+        Sign all transaction inputs. The parameters vary depending upon the way the coins being
+        spent are encumbered.
+        """
         self.Solver(self).sign(*args, **kwargs)
         return self
 
@@ -160,9 +147,38 @@ class Tx(object):
             return False
 
     def is_signature_ok(self, tx_in_idx, *args, **kwargs):
+        """
+        Return a boolean indicating if the given :class:`TxIn` has a correct solution.
+        """
         if len(self.unspents) <= tx_in_idx or self.unspents[tx_in_idx] is None:
             return False
         return self.is_solution_ok(tx_in_idx, *args, **kwargs)
 
     def bad_signature_count(self, *args, **kwargs):
+        "Return a count of how many :class:`TxIn` objects are not correctly solved."
         return sum(0 if self.is_signature_ok(idx, *args, **kwargs) else 1 for idx in range(len(self.txs_in)))
+
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2013 by Richard Kiss
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
