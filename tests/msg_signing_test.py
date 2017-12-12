ad706cea065e7c26e247b379a5534849be77316e
Richard Kiss <him@richardkiss.com>
2017-12-12 15:36:11 +1000

    Use textwrap.dedent to make msg_signing_test less ugly.

--- tests/msg_signing_test.py
+++ tests/msg_signing_test.py
@@ -1,3 +1,4 @@
+import textwrap
 import unittest
 
 from pycoin.contrib.msg_signing import parse_signed_message, verify_message
@@ -80,21 +81,26 @@ HCT1esk/TWlF/o9UNzLDANqsPXntkMErf7erIrjH5IBOZP98cNcmWmnW0GpSAi3wbr6CwpUAN4ctNn1T
     assert ok
 
     # Sampled from: https://www.bitrated.com/u/Bit2c.txt on Sep 3/2014
-    bit2c = '''\
-Username: Bit2c
-Public key: 0396267072e597ad5d043db7c73e13af84a77a7212871f1aade607fb0f2f96e1a8
-Public key address: 15etuU8kwLFCBbCNRsgQTvWgrGWY9829ej
-URL: https://www.bitrated.com/u/Bit2c
+    bit2c = textwrap.dedent(
+        '''
+        Username: Bit2c
+        Public key: 0396267072e597ad5d043db7c73e13af84a77a7212871f1aade607fb0f2f96e1a8
+        Public key address: 15etuU8kwLFCBbCNRsgQTvWgrGWY9829ej
+        URL: https://www.bitrated.com/u/Bit2c
 
------BEGIN BITCOIN SIGNED MESSAGE-----
-We will try to contact both parties to gather information and evidence, and do my best to make rightful judgement. Evidence may be submitted to us on https://www.bit2c.co.il/home/contact or in a private message to info@bit2c.co.il or in any agreed way.
+        -----BEGIN BITCOIN SIGNED MESSAGE-----
+        ''' +
+        'We will try to contact both parties to gather information and evidence, and do my '
+        'best to make rightful judgement. Evidence may be submitted to us on'
+        ' https://www.bit2c.co.il/home/contact or in a private message to info@bit2c.co.il'
+        ' or in any agreed way.' + '''
 
-https://www.bit2c.co.il
------BEGIN SIGNATURE-----
-15etuU8kwLFCBbCNRsgQTvWgrGWY9829ej
-H2utKkquLbyEJamGwUfS9J0kKT4uuMTEr2WX2dPU9YImg4LeRpyjBelrqEqfM4QC8pJ+hVlQgZI5IPpLyRNxvK8=
------END BITCOIN SIGNED MESSAGE-----
-'''
+        https://www.bit2c.co.il
+        -----BEGIN SIGNATURE-----
+        15etuU8kwLFCBbCNRsgQTvWgrGWY9829ej
+        H2utKkquLbyEJamGwUfS9J0kKT4uuMTEr2WX2dPU9YImg4LeRpyjBelrqEqfM4QC8pJ+hVlQgZI5IPpLyRNxvK8=
+        -----END BITCOIN SIGNED MESSAGE-----
+        ''')
     m, a, s = parse_signed_message(bit2c)
     assert a == '15etuU8kwLFCBbCNRsgQTvWgrGWY9829ej'
     assert s == ('H2utKkquLbyEJamGwUfS9J0kKT4uuMTEr2WX2dPU9YI'
@@ -105,37 +111,37 @@ H2utKkquLbyEJamGwUfS9J0kKT4uuMTEr2WX2dPU9YImg4LeRpyjBelrqEqfM4QC8pJ+hVlQgZI5IPpL
     # testnet example
     # Sampled from: http://testnet.bitrated.com/u/bearbin.txt on Sep 3/2014
     # NOTE: Testnet3
-    bearbin = '''\
-Username: bearbin
-Public key: 03fc594c16779054fc5e119c309215c1f40f2ce104b0169cddeb6d20445bd28f67
-Public key address: n2D9XsQX1mDpFGgYqsfmePTy61LJFQnXQM
-URL: http://testnet.bitrated.com/u/bearbin
+    bearbin = textwrap.dedent('''\
+        Username: bearbin
+        Public key: 03fc594c16779054fc5e119c309215c1f40f2ce104b0169cddeb6d20445bd28f67
+        Public key address: n2D9XsQX1mDpFGgYqsfmePTy61LJFQnXQM
+        URL: http://testnet.bitrated.com/u/bearbin
 
------BEGIN BITCOIN SIGNED MESSAGE-----
-Contact
------------
+        -----BEGIN BITCOIN SIGNED MESSAGE-----
+        Contact
+        -----------
 
-bearbin@gmail.com - Email or hangouts (text only).
+        bearbin@gmail.com - Email or hangouts (text only).
 
-/u/bearbin on reddit (slow response, not preferred for use with the service, just for contact).
+        /u/bearbin on reddit (slow response, not preferred for use with the service, just for contact).
 
-Resolution Guidelines:
------------------------------
+        Resolution Guidelines:
+        -----------------------------
 
- * Evidence is needed. (e.g. pictures w/ proof that it's you).
- * If anybody fails to respond, money goes to the other person after 2 weeks.
- * Additional terms available on request.
+         * Evidence is needed. (e.g. pictures w/ proof that it's you).
+         * If anybody fails to respond, money goes to the other person after 2 weeks.
+         * Additional terms available on request.
 
-Pricing
-----------
+        Pricing
+        ----------
 
- * 0.7% Min 0.003 Max 0.15
- * Payment in advance.
------BEGIN SIGNATURE-----
-n2D9XsQX1mDpFGgYqsfmePTy61LJFQnXQM
-IEackZgifpBJs3SqQQ6leUwzvakTZgUKTDuCCn6rVMOQgHlIEzWSYZGQu2H+1chvu68uutzt04cGmsHy/kRIaEc=
------END BITCOIN SIGNED MESSAGE-----
-'''
+         * 0.7% Min 0.003 Max 0.15
+         * Payment in advance.
+        -----BEGIN SIGNATURE-----
+        n2D9XsQX1mDpFGgYqsfmePTy61LJFQnXQM
+        IEackZgifpBJs3SqQQ6leUwzvakTZgUKTDuCCn6rVMOQgHlIEzWSYZGQu2H+1chvu68uutzt04cGmsHy/kRIaEc=
+        -----END BITCOIN SIGNED MESSAGE-----
+        ''')
     m, a, s = parse_signed_message(bearbin)
     assert a == 'n2D9XsQX1mDpFGgYqsfmePTy61LJFQnXQM'
     assert s == ('IEackZgifpBJs3SqQQ6leUwzvakTZgUKTDuCCn6rVMOQgH'
