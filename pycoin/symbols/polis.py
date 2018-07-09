5ca27adf531c4471e2f74d4e5ac3dc9d3b8eb434
cronos <cronos@polispay.org>
2018-07-09 18:55:31 -0500

    Integrate POLIS main net

---
+++ pycoin/symbols/polis.py
@@ -0,0 +1,12 @@
+from pycoin.networks.bitcoinish import create_bitcoinish_network
+
+
+network = create_bitcoinish_network(
+    netcode="POLIS", network_name="Polis", subnet_name="mainnet",
+    wif_prefix_hex="3C", address_prefix_hex="37", pay_to_script_prefix_hex="3C",
+    bip32_prv_prefix_hex="03e25d7e", bip32_pub_prefix_hex="03e25945",
+    magic_header_hex="BD6B0CBF", default_port=24126,
+    dns_bootstrap=[
+        "dnsseed.poliscentral.org", "dnsseed2.poliscentral.org",
+        "dnsseed3.poliscentral.org", "polis.seeds.mn.zone", "polis.mnseeds.com"
+    ])
