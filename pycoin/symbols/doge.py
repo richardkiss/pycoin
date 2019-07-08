5f7490fe4a106a62a554f6bf53907e72288d27fc
Richard Kiss <him@richardkiss.com>
2019-07-08 10:22:27 -0700

    Fix https://github.com/richardkiss/pycoin/issues/332 dogecoin bip32.

--- pycoin/symbols/doge.py
+++ pycoin/symbols/doge.py
@@ -3,4 +3,4 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 network = create_bitcoinish_network(
     symbol="DOGE", network_name="Dogecoin", subnet_name="mainnet",
     wif_prefix_hex="9e", address_prefix_hex="1e", pay_to_script_prefix_hex="16",
-    bip32_prv_prefix_hex="02fd3955", bip32_pub_prefix_hex="02fd3929")
+    bip32_prv_prefix_hex="02fac398", bip32_pub_prefix_hex="02facafd")
