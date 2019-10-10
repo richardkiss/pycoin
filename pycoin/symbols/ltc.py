6e3636da3b75de12c51e3849278dee4e75689ee1
keepkeyjon <35975617+keepkeyjon@users.noreply.github.com>
2019-10-10 07:19:50 -0600

    LTC: use `M` addresses for p2sh
    Closes #352

--- pycoin/symbols/ltc.py
+++ pycoin/symbols/ltc.py
@@ -3,5 +3,5 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 
 network = create_bitcoinish_network(
     network_name="Litecoin", symbol="LTC", subnet_name="mainnet",
-    wif_prefix_hex="b0", sec_prefix="LTCSEC:", address_prefix_hex="30", pay_to_script_prefix_hex="05",
+    wif_prefix_hex="b0", sec_prefix="LTCSEC:", address_prefix_hex="30", pay_to_script_prefix_hex="32",
     bip32_prv_prefix_hex="019d9cfe", bip32_pub_prefix_hex="019da462", bech32_hrp="ltc")
