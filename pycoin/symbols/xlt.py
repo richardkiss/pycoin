c2c5c465dbfd73c44d07f6e8e77697bb337812b6
Pavel Ribalnik <pribalnik@paypal.com>
2021-12-15 14:07:21 +0200

    Support Q addresses for Litecoin testnet

--- pycoin/symbols/xlt.py
+++ pycoin/symbols/xlt.py
@@ -3,5 +3,5 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 
 network = create_bitcoinish_network(
     network_name="Litecoin", symbol="XLT", subnet_name="testnet",
-    wif_prefix_hex="ef", sec_prefix="XLTSEC:", address_prefix_hex="6f", pay_to_script_prefix_hex="c4",
+    wif_prefix_hex="ef", sec_prefix="XLTSEC:", address_prefix_hex="6f", pay_to_script_prefix_hex="3a",
     bip32_prv_prefix_hex="0436ef7d", bip32_pub_prefix_hex="0436f6e1", bech32_hrp="tltc")
