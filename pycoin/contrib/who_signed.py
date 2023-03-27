f43e883fa265efd09ac5981b9f1d5b82738d955d
Richard Kiss <him@richardkiss.com>
2023-03-26 18:38:35 -0700

    Modernize `who_signed` a bit.

--- pycoin/contrib/who_signed.py
+++ pycoin/contrib/who_signed.py
@@ -1,6 +1,7 @@
 from ..encoding.sec import is_sec, public_pair_to_hash160_sec, sec_to_public_pair, EncodingError
 
 from pycoin.coins.SolutionChecker import ScriptError
+from pycoin.intbytes import byte2int
 from pycoin.satoshi.checksigops import parse_signature_blob
 from pycoin.satoshi.der import UnexpectedDER
 
@@ -10,6 +11,8 @@ class WhoSigned(object):
         self._script_tools = script_tools
         self._address = address_api
         self._generator = generator
+        for _ in "CHECKSIG CHECKSIGVERIFY CHECKMULTISIG CHECKMULTISIGVERIFY".split():
+            setattr(self, "OP_%s" % _, byte2int(self._script_tools.compile('OP_%s' % _)))
 
     def solution_blobs(self, tx, tx_in_idx):
         """
@@ -25,39 +28,84 @@ class WhoSigned(object):
         for s in solution_stack:
             yield s
 
-    def extract_signatures(self, tx, tx_in_idx):
-        sc = tx.SolutionChecker(tx)
-        tx_context = sc.tx_context_for_idx(tx_in_idx)
-        # set solution_stack in case there are no results from puzzle_and_solution_iterator
-        solution_stack = []
-        for puzzle_script, solution_stack, flags, sighash_f in sc.puzzle_and_solution_iterator(tx_context):
-            pass
-            # we only care about the last one
-
-        vm = sc.VM(puzzle_script, tx_context, sighash_f, flags=flags, initial_stack=solution_stack[:])
-        for data in self.solution_blobs(tx, tx_in_idx):
+    def _handle_checksig(self, vmc):
+        s = list(vmc.stack)
+        sec_blob = sig_blob = None
+        sig_hash = 0
+        try:
+            sec_blob = vmc.pop()
+            sig_blob = vmc.pop()
             try:
-                sig_pair, sig_type = parse_signature_blob(data)
-                sig_hash = sighash_f(sig_type, sig_blobs=[], vm=vm)
-                yield (data, sig_hash)
-            except (ValueError, TypeError, UnexpectedDER, ScriptError):
-                continue
+                sig_pair, sig_type = parse_signature_blob(sig_blob)
+                sig_hash = vmc.signature_for_hash_type_f(sig_type, [sig_blob], vmc)
+            except ValueError:
+                pass
+        except (IndexError, ValueError):
+            pass
+
+        return ([sec_blob], [(sig_blob, sig_hash)])
+
+    def _handle_checkmultisig(self, vmc):
+        sec_blobs = []
+        sig_blobs = []
+        s = list(vmc.stack)
+        try:
+            key_count = vmc.pop_int()
+            while key_count > 0:
+                key_count -= 1
+                sec_blobs.append(vmc.pop())
+
+            signature_count = vmc.pop_int()
+            while signature_count > 0:
+                signature_count -= 1
+                sig_blob = vmc.pop()
+                sig_hash = 0
+                try:
+                    sig_pair, sig_type = parse_signature_blob(sig_blob)
+                    sig_hash = vmc.signature_for_hash_type_f(sig_type, [sig_blob], vmc)
+                except ValueError:
+                    pass
+                sig_blobs.append((sig_blob, sig_hash))
+        except IndexError:
+            pass
+        vmc.stack = s
+        return (sec_blobs, sig_blobs)
 
     def extract_secs(self, tx, tx_in_idx):
+        for sec_blobs, sig_and_hash_pairs in self.extract_secs_and_signatures(tx, tx_in_idx):
+            for blob in sec_blobs:
+                yield blob
+
+    def extract_signatures(self, tx, tx_in_idx):
+        for sec_blobs, sig_and_hash_pairs in self.extract_secs_and_signatures(tx, tx_in_idx):
+            for sig_blob, sig_hash in sig_and_hash_pairs:
+                try:
+                    sig_pair, sig_type = parse_signature_blob(sig_blob)
+                    yield (sig_blob, sig_hash)
+                except (ValueError, TypeError, UnexpectedDER, ScriptError):
+                    continue
+
+    def extract_secs_and_signatures(self, tx, tx_in_idx):
         """
-        For a given script solution, iterate yield its sec blobs
+        List[Tuple[List[bytes], List[Tuple[bytes, SigHashFType]]]
         """
         sc = tx.SolutionChecker(tx)
         tx_context = sc.tx_context_for_idx(tx_in_idx)
-        # set solution_stack in case there are no results from puzzle_and_solution_iterator
-        solution_stack = []
-        for puzzle_script, solution_stack, flags, sighash_f in sc.puzzle_and_solution_iterator(tx_context):
-            for opcode, data, pc, new_pc in self._script_tools.get_opcodes(puzzle_script):
-                if data and is_sec(data):
-                    yield data
-            for data in solution_stack:
-                if is_sec(data):
-                    yield data
+
+        blobs_for_sig_ops = []
+        def traceback_f(opcode, data, pc, vmc):
+            if opcode in (self.OP_CHECKSIG, self.OP_CHECKSIGVERIFY):
+                blobs_for_sig_ops.append(self._handle_checksig(vmc))
+            if opcode in (self.OP_CHECKMULTISIG, self.OP_CHECKMULTISIGVERIFY):
+                blobs_for_sig_ops.append(self._handle_checkmultisig(vmc))
+            return
+
+        try:
+            sc.check_solution(tx_context, traceback_f=traceback_f)
+        except ScriptError:
+            pass
+
+        return blobs_for_sig_ops
 
     def public_pairs_for_script(self, tx, tx_in_idx, generator):
         """
