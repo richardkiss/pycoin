367a58e25aacf85549a7335f7607ba8a53727c81
Richard Kiss <him@richardkiss.com>
2023-06-03 16:04:44 -0700

    Can now parse LTC blocks with mweb transactions. Mempool mweb transactions don't work yet.

--- pycoin/coins/litecoin/__init__.py
+++ pycoin/coins/litecoin/__init__.py
@@ -1,6 +1,51 @@
-class Tx:
-    pass
+from pycoin.block import Block
+from pycoin.coins.bitcoin.Tx import Tx
+from pycoin.satoshi.satoshi_int import parse_satoshi_int
+from pycoin.satoshi.satoshi_string import parse_satoshi_string
+from pycoin.satoshi.satoshi_struct import parse_struct
 
 
-class Block:
-    pass
+class LTCTx(Tx):
+    @classmethod
+    def parse(class_, f):
+        """Parse a Bitcoin transaction Tx.
+        :param f: a file-like object that contains a binary streamed transaction
+        """
+        (version,) = parse_struct("L", f)
+        v1 = ord(f.read(1))
+        is_segwit = v1 == 0
+        has_mweb = False
+        if is_segwit:
+            flag = ord(f.read(1))
+            if flag == 0:
+                raise ValueError("bad flag in segwit")
+            has_mweb = flag & 0x8 != 0
+            is_segwit = flag & 0x1 != 0
+            v1 = None
+        count = parse_satoshi_int(f, v=v1)
+        txs_in = []
+        for i in range(count):
+            txs_in.append(class_.TxIn.parse(f))
+        count = parse_satoshi_int(f)
+        txs_out = []
+        for i in range(count):
+            txs_out.append(class_.TxOut.parse(f))
+        if is_segwit:
+            for tx_in in txs_in:
+                stack = []
+                count = parse_satoshi_int(f)
+                for i in range(count):
+                    stack.append(parse_satoshi_string(f))
+                tx_in.witness = stack
+        if has_mweb:
+            mweb_tx_type = ord(f.read(1))
+            if mweb_tx_type:
+                # TODO: read serialized MWEB transaction data
+                # TODO: save this away
+                pass
+        (lock_time,) = parse_struct("L", f)
+        return class_(version, txs_in, txs_out, lock_time)
+
+
+class LTCBlock(Block):
+    Tx = LTCTx
--- pycoin/symbols/ltc.py
+++ pycoin/symbols/ltc.py
@@ -1,7 +1,12 @@
+import struct
+
+from pycoin.coins.litecoin import LTCTx, LTCBlock
 from pycoin.networks.bitcoinish import create_bitcoinish_network
+from pycoin.satoshi.satoshi_struct import parse_struct
 
 
 network = create_bitcoinish_network(
     network_name="Litecoin", symbol="LTC", subnet_name="mainnet",
     wif_prefix_hex="b0", sec_prefix="LTCSEC:", address_prefix_hex="30", pay_to_script_prefix_hex="32",
-    bip32_prv_prefix_hex="019d9cfe", bip32_pub_prefix_hex="019da462", bech32_hrp="ltc")
+    bip32_prv_prefix_hex="019d9cfe", bip32_pub_prefix_hex="019da462", bech32_hrp="ltc",
+    block=LTCBlock, tx=LTCTx)
