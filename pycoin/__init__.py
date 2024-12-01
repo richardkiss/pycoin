5362f82fd224a83d379a9cd7315886afa8442da8
Richard Kiss <him@richardkiss.com>
2024-12-01 15:14:45 -0800

    Works with py3.13

--- pycoin/__init__.py
+++ pycoin/__init__.py
@@ -1,16 +1,24 @@
-from pkg_resources import get_distribution, DistributionNotFound
+version = "unknown"
 
 try:
-    version = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    version = "unknown"
+    from pkg_resources import get_distribution
 
-__title__ = 'pycoin'
-__author__ = 'Richard Kiss'
+    version = get_distribution(__name__).version
+except ImportError:
+    try:
+        from importlib import metadata
+
+        version = metadata.version("pycoin")
+    except Exception:
+        pass
+except Exception:
+    pass
+
+__title__ = "pycoin"
+__author__ = "Richard Kiss"
 __version__ = version
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2018 Richard Kiss'
+__license__ = "MIT"
+__copyright__ = "Copyright 2018 Richard Kiss"
 
 """
 :copyright: (c) 2018 by Richard Kiss
